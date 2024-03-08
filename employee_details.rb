d356c5c888f54670f2ee4526bc9fab5bcd80306b
Kishore Cheruku <kishorecheruku@Kishores-MacBook-Pro.local>
2024-03-07 18:01:54 -0600

    Assignment 1 done

---
+++ employee_details.rb
@@ -0,0 +1,12 @@
+puts "Enter employee details:"
+puts "Enter name"
+name = gets.chomp
+puts "Enter age"
+age = gets.chomp
+puts "Enter age"
+address = gets.chomp
+
+puts "Employee Details:"
+puts "Name: #{name}"
+puts "Age: #{age}"
+puts "Address: #{address}"
