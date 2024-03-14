1e030458aecf4b73afab9695a72d126d5a3ad38c
Kishore Cheruku <kishorecheruku@Kishores-MacBook-Pro.local>
2024-03-14 18:32:56 -0500

    Access modifiers monkey patching arrays

---
+++ loops.rb
@@ -0,0 +1,32 @@
+arr = [1, 2, 3, "kishore", "cheruku", 4.5, 6.7, 8.9]
+
+# arr.each do |i|
+#   puts i
+#   puts i
+# end
+
+# arr.each do |i| puts i end
+
+# for loop example
+
+# for i in arr
+#   puts i
+# end
+
+# while loop example
+
+# i = 0
+
+# while i < arr.length
+#   puts arr[i]
+#   i = i + 1
+# end
+
+# until loop example
+
+i = 0
+
+until i >= arr.length
+  puts arr[i]
+  i = i + 1
+end
---
+++ monkey_patching.rb
@@ -0,0 +1,9 @@
+class Integer
+  def +(b)
+    puts "You are hacked"
+  end
+end
+
+
+
+puts "1" + "2"
