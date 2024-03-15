cbbba5cb7cda342ab6747f5190a87901b0709630
anveshk50 <contactanveshk50@gmail.com>
2024-03-15 07:33:02 -0500

    print even and odd numbers

--- assignment5.rb
+++ assignment5.rb
@@ -11,4 +11,27 @@ def length(str)
 end
 
 string_length = length("Kishore")
-puts string_length  
+puts "String length is #{string_length}"  
+
+
+
+#Printing even and odd numbers
+def print_even_and_odd_numbers(array)
+    even_numbers = []
+    odd_numbers = []
+    #looping the array
+    for num in array
+      # checking if number id even or not. Based on check pushing the number into array
+      if num.even?
+        even_numbers.push(num)
+      else
+        odd_numbers.push(num)
+      end
+    end
+  
+    puts "Even numbers: #{even_numbers}"
+    puts "Odd numbers: #{odd_numbers}"
+  end
+
+  numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+  print_even_and_odd_numbers(numbers)
